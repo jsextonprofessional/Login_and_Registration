014648d12da363af1d81485c28fa2d9ddf560b40
jsprofessional <jshuman@protonmail.com>
2021-07-21 13:34:25 -0500

    Added static method to validate user input. Method not complete yet.

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,11 +2,19 @@ import re
 from flask_app import app
 from flask import render_template, request, redirect, session, flash
 from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/users/register')
+def user_registration():
+    print(User.registration_validation(request.form))
+    return redirect('/')
 
 @app.route('/success')
 def successful_login_page():
-    return render_template("success.html")
+    return render_template('success.html')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,9 @@
+import re
+from flask import flash
+from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
 
 class User():
     def __init__(self, data):
@@ -9,3 +14,22 @@ class User():
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @staticmethod
+    def registration_validation(data):
+        is_valid = True
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+    # first name - letters only, 2-45 characters, not already in db and was submitted
+        if len(data['first_name']) < 2 or len(data['first_name']) > 45:
+            flash('First Name should be between 2 and 45 characters.')
+    # last name - same as first name
+        if len(data['last_name']) < 2 or len(data['last_name']) > 45:
+            flash('Last Name should be between 2 and 45 characters.')
+    # email - valid email format, not already in DB, was submitted, not already in db
+        if not EMAIL_REGEX.match(data['email']):
+            flash('Email address invalid, please try again')
+            is_valid = False
+    # pw - at least 8 - 255 char, was submitted
+    # confirm pw - matches pw
+        return is_valid
