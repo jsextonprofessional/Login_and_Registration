03ccd152984e0eb5540975e1a04c9210a21aab06
jsprofessional <jshuman@protonmail.com>
2021-07-21 17:03:27 -0500

    All should be working

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -20,8 +20,31 @@ def user_registration():
             'password': bcrypt.generate_password_hash(request.form['password'])
         }
         User.insert_user(data)
+        return redirect('/success')
     return redirect('/')
+
+@app.route('/users/login', methods=['POST'])
+def user_login():
+    users = User.select_users_by_email(request.form)
+    if len(users) != 1:
+        flash('Account associated with that email address does not exist.')
+        return redirect('/')
+    user = users[0]
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash('Incorrect password.')
+        return redirect('/')
+    session['user_id'] = user.id
+    session['first_name'] = user.first_name
+    return redirect('/success')
 
 @app.route('/success')
 def successful_login_page():
+    if 'user_id' not in session:
+        flash('Must log in to view this page.')
+        return redirect('/')
     return render_template('success.html')
+
+@app.route('/logout')
+def user_logout():
+    session.clear()
+    return redirect('/')
--- server.py
+++ server.py
@@ -4,3 +4,4 @@ from flask_app.controllers import users
 
 if __name__=="__main__":
     app.run(debug=True)
+    app.secret_key = "O-hi-Mark"
